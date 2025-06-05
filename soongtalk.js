04352a1f38ec929250ce48424f7ae0772e77e5c2
handofly <handohyeon12@naver.com>
2025-06-06 00:06:15 +0900

    Update soongtalk.js

--- soongtalk.js
+++ soongtalk.js
@@ -4,6 +4,6 @@ function drawSoongtalk() {
   background(255);
   image(soongtalk, width / 2, height / 2, width, height); 
   
-  soongX = 200;
-  soongY = 500;
+  soongX = width/4;
+  soongY = height * 5/6;
 }
